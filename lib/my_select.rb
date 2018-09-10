d88baa4c7a6d996df2fb4960297ac2b8929a0ca9
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 17:16:49 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,3 @@
 def my_select(arr)
-  if arr != 0
-    i = 0
-    selection = []
-    while i < arr.length
-      if yield(arr[i]) == true
-        selection << yield(arr[i])
-      end
-    end
-    selection
-  end
+  puts "blow it out your ass"
 end
