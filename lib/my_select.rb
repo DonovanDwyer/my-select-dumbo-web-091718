424826bb431f3d1d4309235b2ea07eb92102ccb4
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 17:41:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,5 @@
 def my_select(arr)
-  yield(arr)
-  puts "So #{yield(arr[0])} is crazy right?"
-  puts "What about #{yield(arr[1])}?"
+  yield(arr[0]).even?
 end
 
-my_select([1,2,3,4]) {|y| y if y.even?}
+my_select([1,2,3,4]) {|y| y}
