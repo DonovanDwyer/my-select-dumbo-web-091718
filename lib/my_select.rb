2f855cd959071c5443727de0152a6aaddf2f5ff7
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 17:45:47 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  if block_given?
+    i = 0
+    s = []
+    while i < arr.length
+      s << arr[i] if yield(arr[i])
+    i += 1
+    end
+    s
+  end
 end
