7538c6e60212ff2682ed24693656de5b20826f70
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 17:09:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,12 @@
 def my_select(arr)
-  i = 0
-  selection = []
-  while i < arr.length
-    if yield(arr[i]) == true
-      selection << arr[i]
+  if block_given?
+    i = 0
+    selection = []
+    while i < arr.length
+      if yield(arr[i]) == true
+        selection << arr[i]
+      end
     end
+    selection
   end
-  selection
 end
